406b173d5b13ebf8dc6ccba88b92958f53c84e47
MarioMastr <ashrmoha11@gmail.com>
2026-06-08 11:14:02 +0300

    rename file variable to something more appropriate

--- RSDKv5/RSDK/User/Steam/SteamStorage.cpp
+++ RSDKv5/RSDK/User/Steam/SteamStorage.cpp
@@ -7,9 +7,9 @@ namespace fs = std::filesystem;
 void SKU::SteamUserStorage::CopySave(bool save, const char *filename) {
     char steamFolderPath[0x100];
     if (SteamUser()->GetUserDataFolder(steamFolderPath, sizeof(steamFolderPath))) {
-        std::string file(steamFolderPath);
-        size_t pos = file.find("/local"); // linux stores in .local so we need the /
-        std::string steamFilePath = file.erase(pos).append("/remote/").append(filename);
+        std::string steamFolderString = steamFolderPath;
+        size_t pos = steamFolderString.find("/local"); // linux stores in .local so we need the /
+        std::string steamFilePath = steamFolderString.erase(pos).append("/remote/").append(filename);
 
         if (fs::exists(steamFilePath)) {
             fs::path steamFile = steamFilePath;
